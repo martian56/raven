8212f96550d04d4b828ddf328b3c8a60bb5d0ce2
martian56 <fuadelizade469@gmail.com>
2025-10-04 14:41:39 +0300

    Added support for Enums, Custom types, Resolved optimization-level compilation issues, and more......

--- src/ast.rs
+++ src/ast.rs
@@ -3,9 +3,11 @@ pub enum ASTNode {
     VariableDecl(String, Box<Expression>),                    // old style: let x = 5;
     VariableDeclTyped(String, String, Box<Expression>),       // new style: let x: int = 5;
     FunctionDecl(String, String, Vec<Parameter>, Box<ASTNode>), // name, return_type, params, body
+    StructDecl(String, Vec<StructField>), // name, fields
+    EnumDecl(String, Vec<String>), // name, variants
     ForLoop(Box<ASTNode>, Box<Expression>, Box<ASTNode>, Box<ASTNode>), // init, condition, increment, body
     WhileLoop(Box<Expression>, Box<ASTNode>),
-    Assignment(String, Box<Expression>),
+    Assignment(Box<Expression>, Box<Expression>), // target, value
     IfStatement(
         Box<Expression>,       // The condition expression of the 'if'
         Box<ASTNode>,          // The body of the 'if' block
@@ -16,6 +18,7 @@ pub enum ASTNode {
     Print(Box<Expression>),
     FunctionCall(String, Vec<Expression>), // function_name, arguments (as statement)
     MethodCall(Box<Expression>, String, Vec<Expression>), // object.method(args) (as statement)
+    ExpressionStatement(Expression), // For standalone expressions
     Return(Box<Expression>),
     Import(String, Option<String>), // module_name, optional alias
     ImportSelective(String, Vec<String>), // module_name, selected_items
@@ -31,19 +34,28 @@ pub enum Expression {
     StringLiteral(String),
     Identifier(String),
     BinaryOp(Box<Expression>, Operator, Box<Expression>),
+    UnaryOp(Operator, Box<Expression>), // Unary operators like -x, !x
     FunctionCall(String, Vec<Expression>), // function_name, arguments
     ArrayLiteral(Vec<Expression>), // [1, 2, 3]
     ArrayIndex(Box<Expression>, Box<Expression>), // array[index]
     MethodCall(Box<Expression>, String, Vec<Expression>), // object.method(args)
+    StructInstantiation(String, Vec<(String, Expression)>), // StructName { field1: value1, field2: value2 }
+    FieldAccess(Box<Expression>, String), // object.field
+    EnumVariant(String, String), // EnumName::VariantName
 }
 
 
 #[derive(Debug, Clone)]
 pub enum Operator {
+    // Unary operators
+    UnaryMinus,  // -x
+    Not,         // !x
+    // Binary operators
     Add,
     Subtract,
     Multiply,
     Divide,
+    Modulo,
     Equal,
     NotEqual,
     LessThan,
@@ -63,6 +75,12 @@ pub struct Parameter {
 }
 
 #[derive(Debug, Clone)]
+pub struct StructField {
+    pub name: String,
+    pub field_type: String,
+}
+
+#[derive(Debug, Clone)]
 pub enum Type {
     Int,
     Float,
@@ -70,4 +88,6 @@ pub enum Type {
     String,
     Void,
     Array(Box<Type>), // int[] -> Array(Box::new(Type::Int))
+    Struct(String), // StructName -> Struct("StructName")
+    Enum(String), // EnumName -> Enum("EnumName")
 }
